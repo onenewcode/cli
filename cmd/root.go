c2b26730a3f709c7443a47fc1bd6c5d49a57d1a8
alphonsezhang <lovestudy@qq.com>
2024-07-12 15:33:59 +0800

    Cobra first

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,12 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 	"os"
 )
+
+var name string
 
 // rootCmd 表示调用时不带任何子命令的基本命令
 var rootCmd = &cobra.Command{
@@ -21,6 +24,13 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if name != "" {
+			fmt.Printf("Hello, %s!\n", name)
+		} else {
+			fmt.Println("Hello, World!")
+		}
+	},
 }
 
 // 函数负责执行根命令 rootCmd。当调用 rootCmd.Execute() 时，Cobra 会解析命令行参数，
@@ -36,7 +46,7 @@ func Execute() {
 // init 函数会在程序开始执行之前被调用，可以用来初始化全局变量或执行一些预先设定的操作。
 // 在 Cobra 中，init 函数经常用于定义和初始化命令行标志。
 func init() {
-	// 义了一个布尔类型的标志 toggle，它可以通过 -toggle 或 -t （短标记）在命令行中使用。false 是该标志的默认值，如果在命令行中没有指定，则使用这个默认值。
-	// 最后的字符串 "Help message for toggle" 是当用户请求帮助信息时，与这个标志相关的描述。
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//
+	rootCmd.Flags().StringVarP(&name, "name", "n", "", "Name to greet")
+
 }
